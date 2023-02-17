19d52f0328a19d03be1d787d352ad7f597f64ebd
Lisa Ulricksen <lisa.joshua1.9@gmail.com>
2023-02-17 15:55:18 -0600

    finished 'Using the Model' section of tutorial

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::get('/products', function(){
+  return Product::all();
+});
+
+Route::post('/products', function(){
+  return Product::create([
+    'name' => 'Product One',
+    'slug' => 'product-one',
+    'description' => 'This is Product One :-)',
+    'price' => '99.99'
+  ]);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
